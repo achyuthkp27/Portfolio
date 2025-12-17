e39c32110a610c45a92abc0d23c474348daf9cd5
Achyuth KP <kpachyuthz@gmail.com>
2025-12-17 19:43:55 +0530

    fix: switch to HashRouter for reliable github pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ErrorBoundary from "./components/ErrorBoundary";
-import PremiumLoader from "@/components/PremiumLoader";
-import SmoothScroll from "./components/ui/SmoothScroll";
-import CustomCursor from "./components/ui/CustomCursor";
-import ScrollProgress from "./components/ui/ScrollProgress";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-const queryClient = new QueryClient();
+// ... imports
 
 const App = () => (
   <ErrorBoundary>
@@ -22,7 +15,7 @@ const App = () => (
         <Sonner />
         <CustomCursor />
         <ScrollProgress />
-        <BrowserRouter basename="/Portfolio">
+        <HashRouter>
           <SmoothScroll>
             <Routes>
               <Route path="/" element={<Index />} />
@@ -30,7 +23,7 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Routes>
           </SmoothScroll>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </ErrorBoundary>
